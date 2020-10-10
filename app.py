4c53c41c8a3799b271c0ad610ff20e4815b3ea79
James J <61944590+jamesjaganathan@users.noreply.github.com>
2020-10-10 15:18:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def add_numbers_post():
 	  # --> ['5', '6', '8']
 	  # print(type(request.form['text']))
 	  if request.method == 'GET':
-	  	return render_template('add_numbers.html', weight=weight, text=text)
+	  	return render_template('add_numbers.html')
 	  elif request.method == 'POST':
   	      print(request.form['weight'].split())
 	      #print(request.form)
